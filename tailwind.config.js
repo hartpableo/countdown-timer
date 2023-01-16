b756082ce7c5a890f1e3a6693980cd9c2728206a
Hart <pableoh@gmail.com>
2023-01-16 10:43:00 +0800

    frontend progress

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "fontPrimary": "'Poppins', sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
